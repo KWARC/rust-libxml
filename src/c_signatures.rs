bd5cdf7310350758d855e04388d9f4eb1ff46f50
jfschaefer <j.schaefer@jacobs-university.de>
2015-06-03 16:10:48 +0100

    Support references to nodes

--- src/c_signatures.rs
+++ src/c_signatures.rs
@@ -4,9 +4,13 @@ use libc::{c_char, c_void, c_int};
 
 #[link(name = "xml2")]
 extern "C" {
-    pub fn xmlParseFile(filename: *const c_char) -> *mut c_void;
+    //tree
     pub fn xmlSaveFile(filename: *const c_char, cur: *mut c_void) -> c_int;
     pub fn xmlFreeDoc(cur: *mut c_void);
+    pub fn xmlDocGetRootElement(doc: *const c_void) -> *mut c_void;
+
+    //parser
+    pub fn xmlParseFile(filename: *const c_char) -> *mut c_void;
     pub fn xmlCleanupParser();
 }
 
--- src/tree.rs
+++ src/tree.rs
@@ -5,9 +5,19 @@ use c_signatures::*;
 use std::ffi::CString;
 use libc::{c_void, c_int};
 
+///An xml node
+pub struct XmlNodeRef {
+    ///libxml's xmlNodePtr
+    pub xml_node_ptr : *mut c_void,
+    ///The node is inserted into a document.
+    ///*Warning*: This isn't 100% safe if you have several references
+    ///to a node and unlink one of them. So please be reasonable.
+    pub node_is_inserted : bool,
+}
+
 ///An xml document
 pub struct XmlDoc {
-    ///It's libxml's xmlDocPtr
+    ///libxml's xmlDocPtr
     pub xml_doc_ptr : *mut c_void,  //Can we change the visibility somehow?
 }
 
@@ -32,6 +42,19 @@ impl XmlDoc {
                 return Err(());
             }
             Ok(retval)
+        }
+    }
+    ///Get the root element of the document
+    pub fn get_root_element(&self) -> Result<XmlNodeRef, ()> {
+        unsafe {
+            let node_ptr = xmlDocGetRootElement(self.xml_doc_ptr);
+            if node_ptr.is_null() {
+                return Err(());
+            }
+            Ok(XmlNodeRef {
+                xml_node_ptr : node_ptr,
+                node_is_inserted : true,
+            })
         }
     }
 }
