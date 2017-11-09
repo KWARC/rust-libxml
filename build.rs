eea38eab1fceb57eb555333bea743f4ac5a9ff4e
Andreas Franzén <andreas@devil.se>
2017-11-09 23:03:36 +0100

    Also fix depricated name Config -> Build

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@
 extern crate gcc;
 
 fn main() {
-  gcc::Config::new()
+  gcc::Build::new()
     .file("src/helper_functions.c")
     .include("/usr/include/libxml2")
     .compile("libhelper_functions.a");
