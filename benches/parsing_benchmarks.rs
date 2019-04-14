d709726c00b331a22c845e5da26fafa26430ed19
Deyan Ginev <d.ginev@jacobs-university.de>
2019-04-14 15:46:45 -0400

    bench against the Node implementation in single-thread mode

--- benches/parsing_benchmarks.rs
+++ benches/parsing_benchmarks.rs
@@ -4,12 +4,20 @@ extern crate criterion;
 use criterion::Criterion;
 use libxml::parser::Parser;
 use libxml::readonly::RoNode;
-use libxml::tree::NodeType;
+use libxml::tree::{Node, NodeType};
 use rayon::prelude::*;
 
 // -- workhorse functions
 // not *quite* classic depth-first search, since we keep all children at the current level in memory,
 // but certainly DFS-order for traversal
+
+fn dfs_single_classic(node: Node) -> i32 {
+  1 + node
+    .get_child_nodes()
+    .into_iter()
+    .map(dfs_single_classic)
+    .sum::<i32>()
+}
 
 fn dfs_single(node: RoNode) -> i32 {
   1 + node
@@ -25,6 +33,18 @@ fn dfs_parallel(node: RoNode) -> i32 {
     .into_par_iter()
     .map(dfs_parallel)
     .sum::<i32>()
+}
+
+fn dfs_single_classic_work2(node: Node) -> (i32, usize) {
+  if node.get_type() == Some(NodeType::TextNode) {
+    (1, node.get_content().len())
+  } else {
+    node
+      .get_child_nodes()
+      .into_iter()
+      .map(dfs_single_classic_work2)
+      .fold((1, 0), |acc, x| (acc.0 + x.0, acc.1 + x.1))
+  }
 }
 
 fn dfs_single_work2(node: RoNode) -> (i32, usize) {
@@ -53,11 +73,35 @@ fn dfs_parallel_work2(node: RoNode) -> (i32, usize) {
 }
 
 // --- bencher functions
+// to get big.xml download, unpack and rename:
+// http://www.ins.cwi.nl/projects/xmark/Assets/standard.gz
+// or use your own XML sample
+fn bench_single_thread_classic(c: &mut Criterion) {
+  let parser = Parser::default();
+  let doc = parser.parse_file("benches/big.xml").unwrap();
+  c.bench_function("single thread DFS count", move |b| {
+    b.iter(|| {
+      let root = doc.get_root_element().unwrap();
+      assert_eq!(dfs_single_classic(root), 4_690_647)
+    })
+  });
+}
+
+fn bench_single_thread_classic_work2(c: &mut Criterion) {
+  let parser = Parser::default();
+  let doc = parser.parse_file("benches/big.xml").unwrap();
+  c.bench_function("single thread DFS count+length", move |b| {
+    b.iter(|| {
+      let root = doc.get_root_element().unwrap();
+      assert_eq!(dfs_single_classic_work2(root), (4_690_647, 81_286_567))
+    })
+  });
+}
 
 fn bench_single_thread(c: &mut Criterion) {
   let parser = Parser::default();
   let doc = parser.parse_file("benches/big.xml").unwrap();
-  c.bench_function("single thread DFS count", move |b| {
+  c.bench_function("read-only single thread DFS count", move |b| {
     b.iter(|| {
       let root = doc.get_root_readonly().unwrap();
       assert_eq!(dfs_single(root), 4_690_647)
@@ -68,7 +112,7 @@ fn bench_single_thread(c: &mut Criterion) {
 fn bench_single_thread_work2(c: &mut Criterion) {
   let parser = Parser::default();
   let doc = parser.parse_file("benches/big.xml").unwrap();
-  c.bench_function("single thread DFS count+length", move |b| {
+  c.bench_function("read-only single thread DFS count+length", move |b| {
     b.iter(|| {
       let root = doc.get_root_readonly().unwrap();
       assert_eq!(dfs_single_work2(root), (4_690_647, 81_286_567))
@@ -79,7 +123,7 @@ fn bench_single_thread_work2(c: &mut Criterion) {
 fn bench_multi_thread(c: &mut Criterion) {
   let parser = Parser::default();
   let doc = parser.parse_file("benches/big.xml").unwrap();
-  c.bench_function("multi thread DFS count", move |b| {
+  c.bench_function("read-only multi thread DFS count", move |b| {
     b.iter(|| {
       let root = doc.get_root_readonly().unwrap();
       assert_eq!(dfs_parallel(root), 4_690_647);
@@ -90,7 +134,7 @@ fn bench_multi_thread(c: &mut Criterion) {
 fn bench_multi_thread_work2(c: &mut Criterion) {
   let parser = Parser::default();
   let doc = parser.parse_file("benches/big.xml").unwrap();
-  c.bench_function("multi thread DFS count+length", move |b| {
+  c.bench_function("read-only multi thread DFS count+length", move |b| {
     b.iter(|| {
       let root = doc.get_root_readonly().unwrap();
       assert_eq!(dfs_parallel_work2(root), (4_690_647, 81_286_567))
@@ -100,7 +144,7 @@ fn bench_multi_thread_work2(c: &mut Criterion) {
 criterion_group!(
   name = benches;
   config = Criterion::default().sample_size(10);
-  targets = bench_single_thread, bench_single_thread_work2, bench_multi_thread, bench_multi_thread_work2
+  targets = bench_single_thread_classic,  bench_single_thread_classic_work2, bench_single_thread, bench_single_thread_work2, bench_multi_thread, bench_multi_thread_work2
 );
 
 criterion_main!(benches);
