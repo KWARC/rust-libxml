1f5093c350a664a0ce79445342a7e37615624837
Joshua Nitschke <JoshuaNitschke@gmail.com>
2020-09-28 15:46:16 -0700

    #72-add-vcpkg-optional-dependency
    add opt in dependency for vcpkg
    make pkg-config optional but default

--- build.rs
+++ build.rs
@@ -1,7 +1,44 @@
-use pkg_config::find_library;
+fn main() {  
+  if cfg!(not(feature = "pkg-config")) && cfg!(not(feature = "vcpkg"))
+  {
+    panic!(r#"Enable "pkg-config" or "vcpkg" feature flags to locate libxml2"#)
+  }
 
-fn main() {
-  if find_library("libxml-2.0").is_err() {
-    panic!("Could not find libxml2 using pkg-config")
+  #[cfg(feature = "pkg-config")]
+  {
+    if pkg_config_dep::find() {
+      return;
+    }
+  }
+
+  #[cfg(feature = "vcpkg")]
+  {
+    if vcpkg_dep::find() {
+      return;
+    } 
+  }
+
+  panic!("Could not find libxml2.")
+}
+
+#[cfg(feature = "pkg-config")]
+mod pkg_config_dep {
+  use pkg_config;
+  pub fn find() -> bool {
+    if pkg_config::find_library("libxml-2.0").is_ok() {
+      return true;
+    }
+    false
+  }
+}
+
+#[cfg(feature = "vcpkg")]
+mod vcpkg_dep {
+  use vcpkg;
+  pub fn find() -> bool {
+    if vcpkg::find_package("libxml2").is_ok() {
+      return true
+    }
+    false
   }
 }
