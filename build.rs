b85992fa076cf64380c1f0a6dada72859a52036a
prz@zeta2.ch <prz@zeta2.ch>
2021-05-13 10:58:41 +0200

    use the LIBXML2 variable if set directly instead of figuring out packaging

--- build.rs
+++ build.rs
@@ -1,19 +1,29 @@
 fn main() {
-  #[cfg(any(target_family="unix", target_os="macos"))]
-  {
-    if pkg_config_dep::find() {
-      return;
-    }
-  }
+  if let Ok(ref s) = std::env::var("LIBXML2") {
+      // println!("{:?}", std::env::vars());
+      // panic!("set libxml2.");
+      let p = std::path::Path::new(s);
+      let fname = std::path::Path::new(p.file_name().expect("no file name in LIBXML2 env"));
+      assert!(p.is_file());
+      println!("cargo:rustc-link-lib={}", fname.file_stem().unwrap().to_string_lossy());
+      println!("cargo:rustc-link-search={}", p.parent().expect("no library path in LIBXML2 env").to_string_lossy());
+  } else {
+    #[cfg(any(target_family = "unix", target_os = "macos"))]
+        {
+          if pkg_config_dep::find() {
+            return;
+          }
+        }
 
-  #[cfg(windows)]
-  {
-    if vcpkg_dep::find() {
-      return;
-    }
-  }
+    #[cfg(windows)]
+        {
+          if vcpkg_dep::find() {
+            return;
+          }
+        }
 
-  panic!("Could not find libxml2.")
+    panic!("Could not find libxml2.")
+  }
 }
 
 #[cfg(any(target_family="unix", target_os="macos"))]
