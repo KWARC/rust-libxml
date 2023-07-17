96c91027c607593a640d08778bd34b1b093fce8d
JDSeiler <jseiler@radford.edu>
2023-07-17 19:38:39 -0400

    Incorporate code review suggestions

--- src/schemas/common.rs
+++ src/schemas/common.rs
@@ -13,7 +13,7 @@ pub fn structured_error_handler(ctx: *mut c_void, error: bindings::xmlErrorPtr) 
   assert!(!ctx.is_null());
   let errlog = unsafe { &mut *{ ctx as *mut Vec<StructuredError> } };
 
-  let error = StructuredError::from_raw(error);
+  let error = unsafe { StructuredError::from_raw(error) };
 
   errlog.push(error);
 }
