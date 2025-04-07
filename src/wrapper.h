de95f576e873eaf3646a5ae7959c2e206319567a
Antonin Delpeuch <antonin@delpeuch.eu>
2025-04-07 11:20:36 +0200

    Call bindgen at build time on UNIX platforms
    Fixes #73.

---
+++ src/wrapper.h
@@ -0,0 +1,6 @@
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include <libxml/xpathInternals.h>
+#include <libxml/xmlsave.h>
+#include <libxml/HTMLparser.h>
+#include <libxml/xmlschemas.h>
