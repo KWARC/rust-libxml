2b96e39859125e9b41272377fee7782028fbbe8d
dginev <dginev@users.noreply.github.com>
2025-07-22 11:41:45 +0000

    Automated deployment: Tue Jul 22 11:41:44 UTC 2025 bdbd759e20d41cb56a457cb2a5fe21a363885eef

---
+++ target/debug/build/bindgen-898842bc6bfc77c9/build_script_build-898842bc6bfc77c9.d
@@ -0,0 +1,5 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/bindgen-898842bc6bfc77c9/build_script_build-898842bc6bfc77c9.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bindgen-0.72.0/build.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/bindgen-898842bc6bfc77c9/build_script_build-898842bc6bfc77c9: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bindgen-0.72.0/build.rs
+
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bindgen-0.72.0/build.rs:
---
+++ target/debug/build/libxml-b1c7df5fc9cd044e/build_script_build-b1c7df5fc9cd044e.d
@@ -0,0 +1,5 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-b1c7df5fc9cd044e/build_script_build-b1c7df5fc9cd044e.d: build.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-b1c7df5fc9cd044e/build_script_build-b1c7df5fc9cd044e: build.rs
+
+build.rs:
