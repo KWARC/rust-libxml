203e49ec6b124d2a8c05d9172c5aa5a43cb2e2da
dginev <dginev@users.noreply.github.com>
2025-08-18 16:24:28 +0000

    Automated deployment: Mon Aug 18 16:24:28 UTC 2025 ecfa38c75f7b975034567a67abcee3d54b9476b3

---
+++ target/debug/build/libxml-abf510b073c0e841/build_script_build-abf510b073c0e841.d
@@ -0,0 +1,5 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-abf510b073c0e841/build_script_build-abf510b073c0e841.d: build.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-abf510b073c0e841/build_script_build-abf510b073c0e841: build.rs
+
+build.rs:
