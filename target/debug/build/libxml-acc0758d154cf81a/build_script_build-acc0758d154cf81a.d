68cdc48590d189aecd2abfde90f2103374188571
dginev <dginev@users.noreply.github.com>
2026-06-21 15:02:24 +0000

    Automated deployment: Sun Jun 21 15:02:24 UTC 2026 95f02f36449d32d4b578cdf769370fe580098346

---
+++ target/debug/build/libxml-acc0758d154cf81a/build_script_build-acc0758d154cf81a.d
@@ -0,0 +1,5 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-acc0758d154cf81a/build_script_build-acc0758d154cf81a.d: build.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-acc0758d154cf81a/build_script_build-acc0758d154cf81a: build.rs
+
+build.rs:
---
+++ target/debug/deps/cexpr-c985c2fd2a2f7d9d.d
@@ -0,0 +1,10 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/deps/cexpr-c985c2fd2a2f7d9d.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/expr.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/literal.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/token.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/deps/libcexpr-c985c2fd2a2f7d9d.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/expr.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/literal.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/token.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/deps/libcexpr-c985c2fd2a2f7d9d.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/expr.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/literal.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/token.rs
+
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/lib.rs:
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/expr.rs:
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/literal.rs:
+/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cexpr-0.6.0/src/token.rs:
