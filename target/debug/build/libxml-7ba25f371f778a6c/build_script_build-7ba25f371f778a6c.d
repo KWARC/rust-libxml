73a28e726b80be10050ecffa473d594d7dae15e7
dginev <dginev@users.noreply.github.com>
2026-07-07 00:53:54 +0000

    Automated deployment: Tue Jul  7 00:53:54 UTC 2026 e3323da3cafab18b06b2af0714b000cb685406aa

---
+++ target/debug/build/libxml-7ba25f371f778a6c/build_script_build-7ba25f371f778a6c.d
@@ -0,0 +1,5 @@
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-7ba25f371f778a6c/build_script_build-7ba25f371f778a6c.d: build.rs
+
+/home/runner/work/rust-libxml/rust-libxml/target/debug/build/libxml-7ba25f371f778a6c/build_script_build-7ba25f371f778a6c: build.rs
+
+build.rs:
